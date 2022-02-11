f7e2d0f579be27ddfd001dd52d7efa40124d1c8b
eslerm <eslerm@umn.edu>
2022-02-11 15:47:19 -0600

    navbar projects

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,53 @@
 import "../styles/Navbar.css";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 export default function NavPane(props) {
+  const projects = useSelector((state) => state.user.projects, shallowEqual);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user, shallowEqual);
+
+  async function handleLoadProject(id) {
+    const request = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(`${URL}/api/projects/${id}`, request);
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      dispatch({
+        type: "project/loaded",
+        payload: { id: id, stages: result },
+      });
+      navigate(`../project/${id}`);
+    }
+  }
+
+
+  const listProjects = projects
+    ? projects.map((id) => {
+        return (
+          //TODO 1: Change to project.id
+	  //TODO 2: make onClick work/better
+          //        probably a better way to apply this
+          //TODO 3: set class of currently selected project to "current"
+          //        this will add green color via css
+          //TODO 4: a blank id is generated for the first index?
+          <li key={id}>
+	    <a onClick="{() => handleLoadProject(id)}">
+              {id}
+            </a>
+          </li>
+        );
+      })
+    : null;
+
   return (
     <nav id="navbar">
       <ul class="tabrow">
@@ -10,6 +57,7 @@ export default function NavPane(props) {
         <li>Fab Project 4</li>
         <li>+ Create New Project</li>
       </ul>
+      {projects && <ul class="tabrow">{listProjects}</ul>}
     </nav>
   );
 }
