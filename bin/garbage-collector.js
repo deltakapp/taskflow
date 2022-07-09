62f3d21284a480f4f9432e96cec6c8ff8850a495
Delta Kapp <deltakapp@gmail.com>
2022-07-09 00:12:46 -0700

    garbage-collector v1.0

--- bin/garbage-collector.js
+++ bin/garbage-collector.js
@@ -1,15 +1,18 @@
+/* deletes temporary user profiles from "tempusers" collection */
+
 const mongoose = require("mongoose");
-const { User, TempUser } = require("../server/models/userModel");
 const URL = process.env.DB_URL;
-console.log(URL);
 
 (async () => {
   try {
     await mongoose.connect(URL);
     console.log("Connection established with database");
     const connection = mongoose.connection;
+
     connection.db.collection("tempusers").deleteMany({});
     console.log("Deleted documents in collection");
+
+    process.exit();
   } catch (err) {
     console.log(err);
   }
@@ -18,51 +21,3 @@ console.log(URL);
     console.log(err);
   });
 })();
-
-// /* Connect server to mongodb using mongoose */
-// mongoose
-//   .connect(URL)
-//   .then(() => {
-//     console.log("Connection established with database");
-//     const connection = mongoose.connection;
-//     connection.db.collection("tempusers", function (err, collection) {
-//       collection.deleteMany({});
-//     });
-//     console.log("Deleted documents in collection");
-//   })
-
-//   /* Handle initial connection errors */
-//   .catch((err) => {
-//     console.error("Database connection error: ", err);
-//     app.use((req, res) => {
-//       res.status(503).send("Error: database is temporarily unavailable");
-//     });
-//   });
-
-// /* Handle connection errors after initial connection */
-// mongoose.connection.on("error", (err) => {
-//   console.error(err);
-//   app.use((req, res) => {
-//     res.status(503).send("Error: database is temporarily unavailable");
-//   });
-// });
-
-// (async () => {
-//   try {
-//     console.log("deleting collection");
-//     mongoose.connection.db.tempusers.deleteMany({});
-//     console.log("deleting one user");
-//     await User.findByIdAndDelete("62c903e9ee5a635f12dcc0f0");
-//     console.log("deleting temp users");
-//     await TempUser.findOneAndDelete({
-//       name: "Temporary User",
-//     });
-//     console.log("completed findOneAndDelete");
-//     await TempUser.deleteMany({});
-//     console.log("Deleted temp users");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
-
-// process.exit();
