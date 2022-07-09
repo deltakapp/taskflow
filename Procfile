d56646b941c2894f2a3305d4797dc66bd52f65a0
Delta Kapp <deltakapp@gmail.com>
2022-07-09 00:18:26 -0700

    cemented garbage-collector function

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: bin/qgtunnel npm start
-garbage: node bin/garbage-collector.js
 home: nodemon server/index.js --ignore frontend/
--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -11,31 +11,31 @@ const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 
 /* Create user */
-// router.post("/", async (req, res) => {
-//   try {
-//     const user = new User(req.body);
-//     const token = user.generateAuthToken();
-//     await user.save();
-//     // TODO: sendWelcomeEmail
-//     console.log("user created");
-//     res.status(201).send({ user, token });
-//   } catch (err) {
-//     if (err.code === 11000) {
-//       res
-//         .status(409)
-//         .send("This email address is already registered to another user.");
-//     } else {
-//       res
-//         .status(400)
-//         .send(
-//           "Email must be valid email address and password must be minimum 8 characters."
-//         );
-//     }
-//   }
-// });
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const token = user.generateAuthToken();
+    await user.save();
+    // TODO: sendWelcomeEmail
+    console.log("user created");
+    res.status(201).send({ user, token });
+  } catch (err) {
+    if (err.code === 11000) {
+      res
+        .status(409)
+        .send("This email address is already registered to another user.");
+    } else {
+      res
+        .status(400)
+        .send(
+          "Email must be valid email address and password must be minimum 8 characters."
+        );
+    }
+  }
+});
 
 /* Create placeholder user for users to preview app */
-router.post("/", async (req, res) => {
+router.post("/temp/", async (req, res) => {
   const tempNumber = Math.floor(Math.random() * 10000); // random int < 10000
   const name = `Temporary User`;
   const email = `${tempNumber}@invalidemail.com`;
